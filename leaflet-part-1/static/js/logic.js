b813881f773d706e0ed0e092bac295751bd6e00b
nCervantez <nathaniel.cervantez@yahoo.com>
2023-09-27 21:05:06 -0500

    Adjusted code to change color based on depth, Added depth to the popup on each circle

--- leaflet-part-1/static/js/logic.js
+++ leaflet-part-1/static/js/logic.js
@@ -1,6 +1,7 @@
 //Url for the earthquake data
 let queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
+//Create the initial map
 let myMap = L.map("map", {
   center: [37.09, -95.71],
   zoom: 5
@@ -16,27 +17,40 @@ function markerSize(magnitude) {
   return magnitude * 15000;
 }
 
+//Define a markerFill() function that will color the markers based on the depth of the earthquake.
 function markerFill(depth) {
-  return depth ;
+  
+  if (depth < 10){
+    return '#9BD770';
+  }
+  else if (depth < 30){
+    return '#F1F791';
+  }
+  else if (depth < 50){
+    return '#FDE281';
+  }
+  else if (depth < 70){
+    return '#E9B701';
+  }
+  else if (depth < 90){
+    return '#E48D0C';
+  }
+  else{
+    return '#950E17';
+  };
 }
 
-function Features(eqData) {
-
-  function markerData(features, layer) {
-    layer.bindPopup(`<h3>${features.properties.title}</h3><hr><p>${new Date(features.properties.time)}</p>`);
-  }
-};
-
-  
-  d3.json(queryUrl).then(function (data) {
-      
-    console.log(data.features);
+d3.json(queryUrl).then(function (data) {
     
-  
+    //Log the data into the console to ensure captrure
+    console.log(data.features);
+    //Assign the features dictionary to a var
     let featureData = data.features;
-  
+
+    //Loop to add a circle marker to each earthquake in the dataset
     for (let i = 0; i < featureData.length; i++) {
       
+      //assign data to variables
       const coordinates = featureData[i].geometry.coordinates;
       const magnitude = featureData[i].properties.mag;
 
@@ -44,11 +58,13 @@ function Features(eqData) {
       const latlng = L.latLng(coordinates[1], coordinates[0]);
       
       
-      L.circle(latlng, {
-        fillOpacity: markerFill(coordinates[2]),
-        color: "green",
-        fillColor: "green",
-        radius: markerSize(featureData[i].properties.mag)
-      }).bindPopup(`<h3>${featureData[i].properties.title}</h3><hr><p>${new Date(featureData[i].properties.time)}</p>`).addTo(myMap);
+      //Create a circle marker on the map, bind data onto the popup to provide information about the earthquake recorded
+    let eachMarker = L.circle(latlng, {
+        fillOpacity: 0.75,
+        color: markerFill(coordinates[2]),
+        fillColor: markerFill(coordinates[2]),
+        radius: markerSize(magnitude)
+      }).bindPopup(`<h3>${featureData[i].properties.title}</h3><hr><p>${new Date(featureData[i].properties.time)}</p><p>Depth: ${coordinates[2]}</p>`).addTo(myMap);
     }
+
 });
